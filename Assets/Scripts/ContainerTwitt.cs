aa093c2355488c11e7338035817836eb7a0419c6
Rojoin <nachoarrastua@gmail.com>
2024-01-27 16:46:18 -0300

    Change twitt according to ScriptableObject

--- Assets/Scripts/ContainerTwitt.cs
+++ Assets/Scripts/ContainerTwitt.cs
@@ -1,14 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class ContainerTwitt : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer userIcon;
+    [SerializeField] private Image userIcon;
     [SerializeField] public TextMeshProUGUI userName;
     [SerializeField] public TextMeshProUGUI textContainer;
-    [SerializeField] private SpriteRenderer imageTwitt;
+    [SerializeField] private Image imageTwitt;
     [SerializeField] private Twitt twitt;
+    [SerializeField] private int imageSpacing = 200;
+    [SerializeField] private Vector2 defaultTwittSize;
+
+    private RectTransform _transform;
+
+    private void OnValidate()
+    {
+        _transform = GetComponent<RectTransform>();
+        userIcon.sprite = twitt.userIcon;
+        userName.text = twitt.userName;
+        textContainer.text = twitt.prompt;
+        if (twitt.imageTwitt != null)
+        {
+            imageTwitt.sprite = twitt.imageTwitt;
+            _transform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,defaultTwittSize.y + imageSpacing);
+            imageTwitt.gameObject.SetActive(true);
+        }
+        else
+        {
+            imageTwitt.sprite = null;
+            imageTwitt.gameObject.SetActive(false);
+            _transform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,defaultTwittSize.y);
+        }
+    }
 }
