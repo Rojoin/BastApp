f70978e889622ec467e8255558375f6d857d8764
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-01-28 00:38:41 -0300

    Update Koscu tendency

--- Assets/Scripts/RickTwittSO.cs
+++ Assets/Scripts/RickTwittSO.cs
@@ -6,5 +6,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Create RickTwittSO", fileName = "RickTwittSO", order = 1)]
 public class RickTwittSO : TwittSO
 {
+    [Header("Option prompt")]
+    public string optionPrompt;
+
+    [Header("Possible responses to Ricky")]
     public List<TwittSO> posibleTwitts;
 }
